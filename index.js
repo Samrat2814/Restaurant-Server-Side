108abc8a02a6e854fb3362e191501cc5e6970aad
Samrat2814 <aslamhossainsamrat@gmail.com>
2025-03-30 14:51:45 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+
+
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+
+// Root route
+app.get("/", (req, res) => {
+    res.send("Hello,Server");
+  });
+  
+  // Start the server
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+  
